d256bdb19f357d7e83228bb69c8c6ad3b7cd9f51
i1-kmv <i1-kmv@mail.ru>
2021-08-27 19:24:16 +0200

    fixed feed

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,47 +1,55 @@
 <template>
     <div>
         <div v-if="isLoading">Loading...</div>
-        <div v-if="error">Something bad happened</div>
+        <div v-if="error">Something bad happed</div>
+
         <div v-if="feed">
-            <div class="article-preview" v-for="(article,index) in feed.articles" :key="index">
+            <div
+                    class="article-preview"
+                    v-for="(article, index) in feed.articles"
+                    :key="index"
+            >
                 <div class="article-meta">
-                    <router-link :to="{name:'userProfile', params: {slug: article.author.username} }">
-                        <img :src="article.author.image">
+                    <router-link
+                            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+                    >
+                        <img :src="article.author.image" />
                     </router-link>
                     <div class="info">
-                        <router-link class="author"
-                                     :to="{name:'userProfile', params: {slug: article.author.username} }">
-                            {{article.author.username}}
+                        <router-link
+                                :to="{
+                name: 'userProfile',
+                params: {slug: article.author.username}
+              }"
+                        >
+                            {{ article.author.username }}
                         </router-link>
+                        <span class="date">{{ article.createdAt }}</span>
                     </div>
-                    <span class="date">
-                        {{article.createdAdd}}
-
-                    </span>
+                    <div class="pull-xs-right">ADD TO FAVORITES</div>
                 </div>
-                <div class="pull-sx-right">
-                    ADD TO FAVOURITES
-                </div>
+                <router-link
+                        :to="{name: 'article', params: {slug: article.slug}}"
+                        class="preview-link"
+                >
+                    <h1>{{ article.title }}</h1>
+                    <p>{{ article.description }}</p>
+                    <span>Read more...</span>
+                    TAG LIST
+                </router-link>
             </div>
-            <router-link :to="{name:'article', params:{slug: article.slug}}" class="preview-link">
-                <h1>{{article.title}}</h1>
-                <p>{{article.description}}</p>
-                <span>Read more...</span>
-                TAG LIST
-            </router-link>
+            PAGINATION
         </div>
-        Pagination
-
     </div>
 </template>
 
 <script>
+    import {mapState} from 'vuex'
 
-    import {actionTypes} from "../store/modules/feed"
-    import {mapState} from "vuex";
+    import {actionTypes} from '@/store/modules/feed'
 
     export default {
-        name: "Feed.vue",
+        name: 'McvFeed',
         props: {
             apiUrl: {
                 type: String,
@@ -56,11 +64,8 @@
             })
         },
         mounted() {
+            console.log('feed')
             this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
         }
     }
 </script>
-
-<style scoped>
-
-</style>
