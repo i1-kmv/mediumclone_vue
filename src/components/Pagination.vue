c8549ae9ae068f1e3442e0e961f0cd705b207c9b
i1-kmv <i1-kmv@mail.ru>
2021-08-28 17:01:50 +0200

    create pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,46 @@
+<template>
+    <ul class="pagination">
+        <li
+                v-for="item in pages"
+                :key="item"
+                class="page-item"
+                :class="{active: currentPage === item}"
+        >
+            <router-link :to="{path: url, query: {page: item}}" class="page-link">
+                {{ item }}
+            </router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+    import {range} from '../helpers/utils'
+    export default {
+        name: 'McVPagination',
+        props: {
+            total: {
+                type: Number,
+                required: true
+            },
+            limit: {
+                type: Number,
+                required: true
+            },
+            currentPage: {
+                type: Number,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        },
+        computed: {
+            pages() {
+                const pagesCount = Math.ceil(this.total / this.limit)
+                return range(1, pagesCount)
+            }
+        }
+    }
+</script>
+
